9147a9f88ccac4f40a0f6954f87afb54e412c1ad
hurshh <quasarhrsh@gmail.com>
2021-11-14 23:09:45 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,11 @@
+#HEALTH - E
+-![alt text](https://i.imgur.com/Psax3Iq.png)
+-HEALTH-E is a Flutter app developed by harsh rao and parth thapliyal as part of devHeat hackathon organized by iiitsurat
+-this app serves as a quick link between patients and doctors around world and puts you in touch with a doctor which can help patient in early diagnosis of the situation
+
+##Technologies used
+-Flutter to build the app
+-FireBase authentication to store and manage accounts
+-Firestore to keep track of user profiles and chat data
+
+-![alt text](https://i.imgur.com/H3qg6nJ.png)
--- lib/pages/myHealth_dashboard_page.dart
+++ lib/pages/myHealth_dashboard_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:healthe/constants/colors.dart';
+import 'package:healthe/widgets/buttons.dart';
 
 class MyHealth extends StatefulWidget {
   const MyHealth({Key? key}) : super(key: key);
@@ -12,10 +14,100 @@ class _MyHealthState extends State<MyHealth> {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             padding: EdgeInsets.all(10),
             child: Text('My Health',style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,),),
+          ),
+          Row(
+            children: [
+              Expanded(child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Age',style: TextStyle(fontSize: 20),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      onChanged: (subject){
+
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(12)),
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: myPink),borderRadius: BorderRadius.circular(12)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),),
+              Expanded(child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Weight',style: TextStyle(fontSize: 20),),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      onChanged: (subject){
+
+                      },
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(12)),
+                        focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: myPink),borderRadius: BorderRadius.circular(12)),
+                      ),
+                    ),
+                  ),
+                ],
+              ),)
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('height',style: TextStyle(fontSize: 20),),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  onChanged: (subject){
+
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(12)),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: myPink),borderRadius: BorderRadius.circular(12)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text('Previous medical history',style: TextStyle(fontSize: 20),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              minLines: 8,
+              maxLines: 18,
+              onChanged: (description){
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(12)),
+                focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: myPink),borderRadius: BorderRadius.circular(12)),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AuthenticationPageButton(onPressed: (){}, label: 'Submit', colour: myTurqoise),
           )
         ],
       ),
